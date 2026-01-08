65b808689fc8de91e5dd51457db6cb434ce32752
Hyun <dacapo629@naver.com>
2026-01-08 15:46:21 +0900

    사원CRUD, 이메일 초대버튼

--- src/main/java/boot/team/hr/hyun/emp/controller/EmpController.java
+++ src/main/java/boot/team/hr/hyun/emp/controller/EmpController.java
@@ -1,11 +1,10 @@
-package boot.team.hr.hyun.controller;
+package boot.team.hr.hyun.emp.controller;
 
-import boot.team.hr.hyun.dto.EmpDto;
-import boot.team.hr.hyun.service.EmpService;
+import boot.team.hr.hyun.emp.dto.EmpDto;
+import boot.team.hr.hyun.emp.service.EmpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,9 +17,16 @@ public class EmpController {
     public List<EmpDto> selectAll(){
         return empService.selectAll();
     }
-
     @PostMapping("/insert")
     public void insertEmp(@RequestBody EmpDto empDto){
         empService.insertEmp(empDto);
+    }
+    @PutMapping("/update")
+    public void updateEmp(@RequestBody EmpDto empDto){
+        empService.updateEmp(empDto);
+    }
+    @DeleteMapping("/delete")
+    public void deleteEmpById(@RequestBody EmpDto empDto){
+        empService.deleteEmp(empDto.getEmpId());
     }
 }
